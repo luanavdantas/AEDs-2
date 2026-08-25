50112caec3e0c21d1494985ab1e1ceb1536246f1
Luana Dantas <luana.vsdantas@gmail.com>
2026-08-25 15:59:14 -0300

    .

--- TP1/substring_mais_longa_iterativo.c
+++ TP1/substring_mais_longa_iterativo.c
@@ -5,80 +5,79 @@
     Objetivo: Método iterativo que verifica o tamanho da maior substring
     Autora: Luana Dantas
 */
+int defineTamanho (char string[])
+{
+    int tam=0;
+    //Determinação do tamanho da string de entrada
+    for(int i=0; string[i]!='\0'; i++)
+            tam++;
+    return tam;
+}
 int main()
 {
     char stringPrincipal[100], substring[100];
-    int maior=1, cont, tamStringPrincipal=0, sinal=0, tamSubstring=0, posiSubstring=1;
-    for(int n=0; n<99; n++)
-        stringPrincipal[n]='.';
-    stringPrincipal[99]='\0';
-    for(int p=0; p<99; p++)
-        substring[p]='.';
-    substring[99]='\0';
+    int maior=1, cont, tamStringPrincipal=0, sinal=0, tamSubstring=0, posiSubstring=0, indiceRepetida;
+    for(int i=0; i<100; i++)
+    {
+        stringPrincipal[i] = '\0';
+        substring[i]='\0';
+    }
     scanf("%s",stringPrincipal);
-    //printf("String: %s\n",stringPrincipal);
-    //printf("Substring antes de tudo: %s\n",substring);
+    printf("String: %s\n",stringPrincipal);
+    printf("Substring antes de tudo: %s\n",substring);
     while(!(stringPrincipal[0]=='F' && stringPrincipal[1]=='I' && stringPrincipal[2]=='M' && stringPrincipal[3]=='\0'))
     {
-        for(int i=0; stringPrincipal[i]!='\0'; i++)
-            tamStringPrincipal++;
-        substring[0]=stringPrincipal[0];
-        tamSubstring = 1;
-        //printf("Caractere na primeira posicao: %s\n",substring);
-        for(int j=1; j<tamStringPrincipal; j++)
+        tamStringPrincipal = defineTamanho(stringPrincipal);
+        tamSubstring = defineTamanho(substring);
+        for(int j=0; j<tamStringPrincipal; j++)
         {
-            cont=1;
+            cont=0;
             for(int k=0; k<tamSubstring; k++)
             {
                 if(stringPrincipal[j]!=substring[k]) 
                 {
                     cont++;
-                    //printf("%c diferente de %c\n",stringPrincipal[j], substring[k]);
-                    //printf("Contador atualizado: %d\n",cont);
+                    printf("%c diferente de %c\n",stringPrincipal[j], substring[k]);
                 }
                 else
                 {
-                    k=j;
-                    sinal=1;
+                    indiceRepetida = k;
+                    printf("A letra repetida esta na posicao %d da substring\n",indiceRepetida);
+                    k=tamSubstring;
                 }
             }
-            if(sinal==1) 
+            if(cont==tamSubstring) 
             {
-                //printf("Sinal: %d (caractere igual foi encontrado)\n",sinal);
-                for(int m=0; m<99; m++)
-                    substring[m]='.';
-                substring[99]='\0';
-                substring[0] = stringPrincipal[j];
-                //printf("Substring recomecou: %s\n",substring);
-                tamSubstring=1;
-                //printf("Novo tamanho da substring: %d\n", tamSubstring);
-                posiSubstring=1;
-                //printf("Proxima posicao da substring: %d\n",posiSubstring);
-                sinal=0;
+                printf("Não tem repeticao na substring\n");
+                substring[posiSubstring]=stringPrincipal[j];
+                posiSubstring++;
+                printf("Substring atualizada: %s\n",substring);
+                tamSubstring = defineTamanho(substring);
+                printf("Novo tamanho da substring: %d\n", tamSubstring);
             }
             else
             {
-                substring[posiSubstring]=stringPrincipal[j];
-                //printf("Substring atualizada: %s\n",substring);
-                tamSubstring++;
-                //printf("Novo tamanho da substring: %d\n", tamSubstring);
-                posiSubstring++;
-                //printf("Proxima posicao da substring: %d\n",posiSubstring);
-                //printf("Caractere na posicao atual: %c\n",substring[posiSubstring-1]);
+                for(int i=0; i<100; i++)
+                    substring[i]='\0';
+                posiSubstring=0;
+                substring[posiSubstring] = stringPrincipal[indiceRepetida];
+                //posiSubstring++;
+                j=indiceRepetida+1;
+                printf("Substring recomecou: %s\n",substring);
+                printf("J agora eh %d\n",j);
+                tamSubstring = defineTamanho(substring);
+                printf("Novo tamanho da substring: %d\n", tamSubstring);
             }
-            if(cont>maior) maior=cont;
+            if(tamSubstring>maior) maior=tamSubstring;
         }
         printf("%d\n",maior);
-        for(int n=0; n<99; n++)
-            stringPrincipal[n]='.';
-        stringPrincipal[99]='\0';
-        for(int p=0; p<99; p++)
-            substring[p]='.';
-        substring[99]='\0';
+        for(int i=0; i<100; i++)
+        {
+            stringPrincipal[i] = '\0';
+            substring[i]='\0';
+        }
         maior=1;
-        tamStringPrincipal=0; 
-        sinal=0;
-        posiSubstring=1;
+        posiSubstring=0;
         scanf("%s",stringPrincipal);
     }
     return 0;
