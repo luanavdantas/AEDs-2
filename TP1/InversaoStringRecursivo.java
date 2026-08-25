d5fa41443b9da73f672ac4e0ac4be3b9e872dc3b
Luana Dantas <luana.vsdantas@gmail.com>
2026-08-25 09:52:46 -0300

    .

--- TP1/InversaoStringRecursivo.java
+++ TP1/InversaoStringRecursivo.java
@@ -6,30 +6,32 @@ import java.util.*;
     Objetivo: Método recursivo que invere uma string
 */
 public class InversaoStringRecursivo {
-    public static void inverte (String s, int i)
+    public static String inverte (String s, int i)
     {
-        char letra;
         int tam = s.length();
         //Condição de parada que significa o fim da string
-        if(i==s.length()) return;
-        //Não consegui pensar em um jeito de retornar um vetor de char 
-        //que recebe as letras alteradas então já printo o resultado no próprio método
-        letra = s.charAt(tam-i-1); 
-        System.out.print(letra); //Exibe a string, letra a letra, de trás para frente
-        inverte(s,i+1); //Chamada recursiva
+        if(i==s.length()) return "";
+        return s.charAt(tam-i-1) + inverte(s,i+1); //Chamada recursiva
     }
-    public static void chamaInverte (String s)
+    public static String chamaInverte (String s)
     {
-        inverte(s, 0);
+        return inverte(s, 0);
+    }
+    public static boolean comparar(String nova)
+    {
+        boolean resp;
+        if(nova.charAt(0)=='F' && nova.charAt(1)=='I' && nova.charAt(2)=='M' && nova.length()==3) resp = false;
+        else resp = true;
+        return resp;
     }
     public static void main(String[] args) {
        Scanner sc = new Scanner(System.in);
        String str;
-       while(sc.hasNext())
+       str = sc.nextLine();
+       while(comparar(str))
        {
+            System.out.println(chamaInverte(str));
             str = sc.nextLine();
-            chamaInverte(str);
-            System.out.println();
        }
        sc.close(); 
     }
